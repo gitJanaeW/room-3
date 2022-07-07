1ca76eecd78da7c73eb91ea1d1c37d16c21fd495
janae.wel <janae.wel@gmail.com>
2022-07-07 12:11:56 -0400

    connecting tailwind css framework

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// THIS IS HOW WE ACCESS THE TAILWINDCSS FRAMEWORK.
+// DON'T EDIT, DELETE OR MOVE THIS FILE PLEASE :D
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
